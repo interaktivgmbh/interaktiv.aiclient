6b8c730a2000c3ae862c82faa81d22253ee71239
Alexander Rybakov <rybakov@interaktiv.de>
2025-11-24 14:22:56 +0100

    added volto control panel

--- src/interaktiv/aiclient/controlpanels/client_settings.py
+++ src/interaktiv/aiclient/controlpanels/client_settings.py
@@ -3,7 +3,9 @@ from plone import schema
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from plone.autoform import directives
+from plone.restapi.controlpanels import RegistryConfigletPanel
 from plone.z3cform import layout
+from zope.component import adapter
 from zope.interface import Interface
 
 
@@ -34,6 +36,16 @@ class AIClientForm(RegistryEditForm):
     schema = IAIClientSettings
     schema_prefix = "interaktiv.aiclient"
     label = _("AI Client Settings")
+
+
+@adapter(Interface, Interface)
+class AIClientConfigletPanel(RegistryConfigletPanel):
+    schema = IAIClientSettings
+    schema_prefix = "interaktiv.aiclient"
+    configlet_id = "aiclient-controlpanel"
+    configlet_category_id = "Products"
+    title = "AI Client"
+    group = "Products"
 
 
 AIClientView = layout.wrap_form(AIClientForm, ControlPanelFormWrapper)
