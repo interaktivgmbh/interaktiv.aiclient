0098a9af8b6d4a22fee6e2696ddf28883f318d0a
Alexander Rybakov <rybakov@interaktiv.de>
2025-12-05 15:56:24 +0100

    added more testing

--- tests/client/test_aiclient.py
+++ tests/client/test_aiclient.py
@@ -1,6 +1,8 @@
 from interaktiv.aiclient.client import AIClient
 from interaktiv.aiclient.client import AIClientInitializationError
 from interaktiv.aiclient.interfaces import IAIClient
+from openai import APIStatusError, APITimeoutError, APIConnectionError
+from openai import RateLimitError, BadRequestError, InternalServerError
 from plone import api
 from unittest import mock
 from zope.component import getUtility
@@ -55,3 +57,46 @@ class TestAIClient:
 
         # post condition
         assert res == "Hello world!"
+        assert ai_client.selected_model == "google/gemini-2.5-flash-image"
+
+    # noinspection PyUnusedLocal
+    @mock.patch("interaktiv.aiclient.client.OpenAI")
+    def test_client_handles_errors(self, mock_openai, portal):
+        # setup
+        ai_client: AIClient = getUtility(IAIClient)
+
+        api.portal.set_registry_record(
+            "interaktiv.aiclient.openrouter_api_key", "api_key"
+        )
+        api.portal.set_registry_record(
+            "interaktiv.aiclient.openrouter_model", "google/gemini-2.5-flash-image"
+        )
+
+        # create new client instance with the new mocked OpenAI client
+        ai_client.reload()
+
+        api_status_error_params = {
+            "message": "Test error",
+            "response": mock.MagicMock(),
+            "body": None,
+        }
+
+        errors = {
+            APIStatusError: api_status_error_params,
+            APITimeoutError: { "request": mock.MagicMock() },
+            APIConnectionError: { "message": "Test error", "request": mock.MagicMock() },
+            RateLimitError: api_status_error_params,
+            BadRequestError: api_status_error_params,
+            InternalServerError: api_status_error_params,
+        }
+
+        # do it
+        for error_cls, params in errors.items():
+            mock_client_instance = mock_openai.return_value
+            mock_client_instance.chat.completions.create.side_effect = error_cls(**params)
+
+            # this should not raise
+            res = ai_client.call([{"role": "user", "content": "Hello!"}])
+
+            # post condition
+            assert res is None
