a984c8ccd650b3f5377f66ab61d00187ac0ce733
Alexander Rybakov <rybakov@interaktiv.de>
2026-01-30 15:37:42 +0100

    add more useful description for max concurrent requests field

--- src/interaktiv/aiclient/controlpanels/client_settings.py
+++ src/interaktiv/aiclient/controlpanels/client_settings.py
@@ -32,7 +32,11 @@ class IAIClientSettings(Interface):
 
     max_concurrent_requests = schema.Int(
         title=_("Maximum amount of concurrent requests"),
-        description=_("Maximum amount of concurrent requests when processing a batch."),
+        description=_(
+            "Maximum amount of concurrent requests when processing a batch. "
+            "If you see a lot of retries in the logs when running prompts "
+            "in batch, you should reduce this value."
+        ),
         default=5,
     )
 
