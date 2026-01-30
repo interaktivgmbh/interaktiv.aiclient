0ac05d8649f6537ea96e42d4dbb56e926f8c751d
Alexander Rybakov <rybakov@interaktiv.de>
2026-01-30 11:45:02 +0100

    linting

--- src/interaktiv/aiclient/types.py
+++ src/interaktiv/aiclient/types.py
@@ -1,6 +1,8 @@
-from typing import List, Optional
-from typing_extensions import TypeAlias
 from langchain_core.messages.base import BaseMessage
+from typing import List
+from typing import Optional
+from typing_extensions import TypeAlias
+
 
 Prompt: TypeAlias = List[BaseMessage]
 BatchPrompts: TypeAlias = List[Optional[Prompt]]
